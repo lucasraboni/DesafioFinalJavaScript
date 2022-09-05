9f51f2cfed9074f265086f16483bb2ade96a4172
lucasraboni <lucasraboni7@gmail.com>
2022-09-05 20:33:04 -0300

    Segundo Desafio

--- funciones.js
+++ funciones.js
@@ -5,20 +5,55 @@ let totalCompra = 0
 let sectorSeleccionado = parseInt(prompt('Selecciona el sector del estadio que desee: 1. Popular Este - 2. Popular Oeste - 3. Platea Norte - 4. Platea Sur - 5. Platea Sur Alta - 6. Visitantes'))
 let seguirComprando = true
 let decision
+let productos = []
+let carrito = []
 
-while(seguirComprando===true){
-if(sectorSeleccionado===1){
-    totalCompra = totalCompra + 2000
-} else if(sectorSeleccionado===2){
-    totalCompra = totalCompra + 2500
-}else if(sectorSeleccionado===3){
-    totalCompra = totalCompra + 6500
-}else if(sectorSeleccionado===4){
-    totalCompra = totalCompra + 4700
-}else if(sectorSeleccionado===5){
-    totalCompra = totalCompra + 3000
-}else if(sectorSeleccionado===6){
-    totalCompra = totalCompra + 14500
+const popularEste = {
+    nombre:'Popular Este',
+    precio: 2000
+  }
+productos.push(popularEste)
+const popularOeste = {
+    nombre:'Popular Oeste',
+    precio: 2500,
+}
+  productos.push(popularOeste)
+  const plateaNorte = {
+    nombre:'Platea Norte',
+    precio: 6500
+}
+productos.push(plateaNorte)
+const plateaSur = {
+    nombre:'Platea Sur',
+    precio: 4700
+}
+productos.push(plateaSur)
+const plateaSurAlta = {
+    nombre: 'Platea Sur Alta',
+    precio: 3000
+}
+productos.push(plateaSurAlta)
+const visitantes = {
+    nombre: 'Visitantes',
+    precio: 14500
+}
+productos.push(visitantes)
+
+console.log(productos)
+
+while(seguirComprando === true){
+if(sectorSeleccionado === 1) {
+    carrito.push(productos[0])
+} else if(sectorSeleccionado === 2) {
+    carrito.push(productos[1])
+}else if(sectorSeleccionado === 3) {
+    carrito.push(productos[2])
+}else if(sectorSeleccionado === 4) {
+    carrito.push(productos[3])
+}else if(sectorSeleccionado === 5) {
+    carrito.push(productos[4])
+}else if(sectorSeleccionado === 6) {
+    carrito.push(productos[5])
     alert('El sector visitante abona un impuesto')
 } else{
     sectorSeleccionado = parseInt(prompt('Sector no valido, por favor ingrese un sector existente: 1. Popular Este - 2. Popular Oeste - 3. Platea Norte - 4. Platea Sur - 5. Platea Sur Alta - 6. Visitantes'))
@@ -32,6 +67,17 @@ if (decision === 1) {
     seguirComprando = false
 }
 }
+
+console.log(carrito)
+
+for(let i=0;i<carrito.length;i++){
+    totalCompra = totalCompra +carrito[i].precio
+}
+
+for (const prod of carrito) {
+    totalCompra = totalCompra +prod.precio
+}
+
 
 alert('El precio de las entradas es: ' +totalCompra)
 
@@ -52,6 +98,7 @@ let valorDescuentoSocios = valor *(descuentoSocios/100)
 valor = valor - valorDescuentoSocios
 return valor
 }
+
 let valorConDescuentoSocios = calcularDescuentoDeSocios(totalCompra)
 alert('Total con descuentos siendo socios es: ' +valorConDescuentoSocios)
 
