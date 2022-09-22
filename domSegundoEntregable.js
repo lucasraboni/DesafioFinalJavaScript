21ce17af98b433d683f3eb31d9f7627f8295e4af
lucasraboni <lucasraboni7@gmail.com>
2022-09-21 23:41:36 -0300

    segunda entrega del proyecto final

--- domSegundoEntregable.js
+++ domSegundoEntregable.js
@@ -1,32 +1,73 @@
 let sector = []
-
-const selectTag = document.getElementById('lista')
+let carrito = []
 
 class Sectores{
-    constructor(id,nombre,precio,imagen){
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.imagen = imagen;
-    }
+  constructor(id,nombre,precio,imagen){
+    this.id = id
+    this.nombre = nombre
+    this.precio = precio
+    this.imagen = imagen
+  }
+  desplegarSector(){
+    const card = `
+      <div class="card">
+          <p>${this.nombre}</p>
+          <div>
+              <img class='imgProducto' src=${this.imagen} alt="foto del sector"/>
+          </div>
+          <div>
+              <p>$${this.precio}</p>
+          </div>
+          <div class="btn-container">
+              <button id=${this.id} class='btnAgregar'>AGRGEGAR AL CARRITO</button>
+          </div>
+      </div>
+    `
+    const container = document.getElementById('container')
+    container.innerHTML += card
+  }
+  agregarEvento(){
+      const btnAgregar = document.getElementById(this.id)
+      const sectorEncontrado = sector.find(secto => secto.id == this.id)
+      btnAgregar.addEventListener('click', () => agregarAlCarrito(sectorEncontrado))
+  }    
 }
 
-let SectorEste = new Sectores (1,"Popular Este",2000)
-let SectorOeste = new Sectores (2,"Popular Oeste",2500)
-let SectorNorte = new Sectores (3,"Platea Norte",6500)
-let SectorSur = new Sectores (4,"Platea Sur",4700)
-let SectorSurAlta = new Sectores (5,"Platea Sur Alta",3000)
-let SectorVisitante = new Sectores (5,"Visitante",14500)
-sector.push (SectorEste)
-sector.push (SectorOeste)
-sector.push (SectorNorte)
-sector.push (SectorSur)
-sector.push (SectorSurAlta)
-sector.push (SectorVisitante)
+let SectorEste = new Sectores (001, 'Popular Este', 2000, './multimedias/popularEste.jpg')
+let SectorOeste = new Sectores (002, 'Popular Oeste', 2500, './multimedias/PopularOeste.jpg')
+let SectorNorte = new Sectores (003, 'Platea Norte', 6500, './multimedias/PlateaNorteBaja.jpg')
+let SectorSur = new Sectores (004, 'Platea Sur', 4700, './multimedias/PlateaSurBaja.jpg')
+let SectorSurAlta = new Sectores (005, 'Platea Sur Alta', 3000, './multimedias/PlateaSurAlta.jpg')
+let SectorVisitante = new Sectores (006, 'Visitante', 14500, './multimedias/sectorVisitantes.jpg')
 
+sector.push(SectorEste, SectorOeste, SectorNorte, SectorSur, SectorSurAlta, SectorVisitante)
 
-console.log(sector)
+sector.forEach(e => {
+  e.desplegarSector()
+}) 
+sector.forEach(e => {
+  e.agregarEvento()
+})
 
+function agregarAlCarrito(Sectores) {
+
+  let enCarrito = carrito.find(sect => sect.id == Sectores.id)
+  
+  if(!enCarrito){
+      carrito.push({...Sectores, cantidad: 1})
+  } else {
+      let carritoFiltrado = carrito.filter(sect => sect.id != Sectores.id)
+      carrito = [
+          ...carritoFiltrado, 
+          { ...enCarrito, cantidad: enCarrito.cantidad + 1}
+      ]
+  }
+
+  contador.innerHTML =  carrito.reduce((acc, sect) => acc + sect.cantidad, 0)
+  
+}
+const contador = document.getElementById('cartCounter')
+contador.innerHTML = carrito.reduce((acc, sect) => acc + sect.cantidad, 0)
 sector.forEach((sector) => {
   const option = document.createElement('option')
   option.innerText = `${sector.nombre}: $${sector.precio}`
@@ -34,29 +75,5 @@ sector.forEach((sector) => {
   selectTag.append(option)
 })
 
-let carrito = []
 
-const boton = document.createElement('button')
-boton.innerText = 'SEGUIR COMPRANDO'
-document.body.append(boton)
 
-const boton2 = document.createElement('button')
-boton2.innerText = 'TERMINAR COMPRA'
-document.body.append(boton2)
-
-boton.onclick = () => {
-  const sectorSeleccionado = sector[selectTag.selectedIndex]
-  carrito.push(sectorSeleccionado)
-}
-
-boton2.onclick = () => {
-  console.log(carrito)
-  let totalCompra = 0
-  carrito.forEach((sect) => {
-    totalCompra = totalCompra + sect.precio
-  })
-
-  const p = document.createElement('p')
-  p.innerText = `El precio total a pagar es ${totalCompra}`
-  document.body.append(p)
-}
