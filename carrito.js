4f431ecb9fc282a2661793caf82044521655c104
lucasraboni <lucasraboni7@gmail.com>
2022-10-23 16:42:43 -0300

    Entrega desafio final JavaScript LucasRaboni

--- carrito.js
+++ carrito.js
@@ -1,32 +1,83 @@
-let carrito = JSON.parse(localStorage.getItem('carrito')) ? JSON.parse(localStorage.getItem('carrito')) : []
-let desplegarCarrito = document.getElementById('carrito')
-let precioTotal = document.querySelector('#precio')
-let botonVaciar = document.getElementById('carrito')
-console.log(carrito)
+let carrito = [];
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("carrito")) {
+    carrito = JSON.parse(localStorage.getItem("carrito"));
+    actualizarCarrito();
+  }
+});
+let desplegarCarrito = document.getElementById("carrito");
+let precioTotal = document.querySelector("#precio");
+let botonVaciar = document.getElementById("carrito");
+const vaciarCarrito = document.querySelector("#vaciarCarrito");
+const comprarTickets = document.querySelector("#comprarTickets");
+const cartCounter = document.querySelector("#cartCounter");
 
-carrito.forEach((prod) => {
-    const {nombre, precio, imagen, id, cantidad} = prod 
-    console.log(nombre)
-      const div = document.createElement('div')
-      div.className = ('productoEnCarrito')
-      div.innerHTML = `
+vaciarCarrito.addEventListener("click", () => {
+  carrito.length = [];
+  actualizarCarrito();
+  guardarStorage();
+  arreglarStorage();
+});
+
+comprarTickets.addEventListener("click", () => {
+  Swal.fire({
+    title: "Exelente",
+    text: "Compra finalizada con exitos",
+    icon: "success",
+    background: "#1a75b7",
+    iconColor: "white",
+    color: "white",
+    showCancelButton: false,
+    showConfirmButton: false,
+    timer: 5000,
+  });
+});
+function actualizarCarrito() {
+  desplegarCarrito.innerHTML = "";
+
+  carrito.forEach((prod) => {
+    const { nombre, precio, imagen, id, cantidad } = prod;
+    const div = document.createElement("div");
+    div.classList.add("col-md-3");
+    div.innerHTML = `
       <div class="card" style="width: 18rem;">
       <img class="card-img-top" src="${imagen}" alt="Card image cap">
       <div class="card-body">
       <h5 class="card-title">${nombre}</h5>
       <p class="card-text">Precio: ${precio}</p>
       <p class="card-text"> Cantidad: ${cantidad}</p>
-      <button class="delete btn btn-danger">Borrar</button>  
+      <button class="delete btn btn-color" onclick=borrarProducto(${id})>Borrar</button>  
       </div>
-      `
+      `;
 
+    desplegarCarrito.appendChild(div);
 
-     desplegarCarrito.appendChild(div)
-     console.log(desplegarCarrito)
-    
-     localStorage.setItem('carrito', JSON.stringify(carrito))
-     const precioSuma =  carrito.reduce((acc, prod) => acc + cantidad * prod.precio, 0)
-    precioTotal.textContent = precioSuma 
+    guardarStorage();
+    const precioSuma = carrito.reduce(
+      (acc, prod) => acc + cantidad * prod.precio,
+      0
+    );
+    if (carrito.length === 0) {
+      precioTotal.innerHTML = `<p>No hay elementos</p>`;
+    } else {
+      precioTotal.innerHTML = `El precio total es ${precioSuma}`;
+    }
+  });
+}
 
-})
+function borrarProducto(id) {
+  carrito = carrito.filter((producto) => producto.id !== id);
+  actualizarCarrito();
+  guardarStorage();
+  arreglarStorage();
+}
 
+function guardarStorage() {
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+function arreglarStorage() {
+  if (carrito.length === 0) {
+    localStorage.clear();
+  }
+}
